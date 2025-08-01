45ae0865bc75d9bcb6235ddfe9aa2a9f4ef84552
Aditya Raj <raj.adi@northeastern.edu>
2025-07-31 20:26:35 -0400

    adding HW 07-30-2025
    adding HW 07-30-2025
    
    * adding HW-07-30-2025

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ToDoList from './components/toDoList/ToDoList.jsx'
+import CourseCatalogApp from './components/Course/CourseCatalogApp.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ToDoList/>
+    // <ToDoList/>
+    <CourseCatalogApp/>
+
   )
 }
 
