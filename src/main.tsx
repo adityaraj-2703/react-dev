1a1696baa9148b5f74c930cb49ecf9d35cacecab
Aditya Raj <raj.adi@northeastern.edu>
2025-08-07 13:51:16 -0400

    adding store and middleware
    adding store and middleware

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { TodoProvider } from './context/ToDoContext.jsx';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TodoProvider>
+    <Provider store={store}>
+      <TodoProvider>
       <App />
     </TodoProvider>
+    </Provider>
+    
   </StrictMode>,
 )
+
